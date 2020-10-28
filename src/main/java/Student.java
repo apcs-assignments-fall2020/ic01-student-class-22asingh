c690aec32b1a4e795964eca350b61c0bbcbbc938
22asingh <22asingh@athenian.org>
2020-10-28 19:28:29 +0000

    Created student class by inheriting from Person class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,3 +1,34 @@
-public class Student {
-    
+public class Student extends Person{
+    //instance variable
+    private int classYear;
+
+    //constructor
+    public Student(String a, String b, int c){
+        super(a,b);
+        this.classYear = c;
+    }
+
+    //default constructor
+    public Student(){
+        super("Jerry","Seinfeld");
+        this.classYear = 2024;
+    }
+
+    //toString
+    public String toString(){
+        return this.getFirstName() + " " + this.getLastName() + ", " + this.classYear;
+    }
+
+    //getter
+    public int getClassYear(){
+        return this.classYear;
+    }
+
+    //setter
+    public void setClassYear(int x){
+        if (classYear > 2020 && classYear <= 2025){
+            this.classYear = x;
+        }
+    }
+
 }
